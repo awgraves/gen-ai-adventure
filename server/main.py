1959da38e67b7515b0e98e5a759a1bc3d5ed9d6e
Andrew Graves <agraves@codecademy.com>
2024-12-17 17:54:00 -0500

    load API_KEY from env

--- server/main.py
+++ server/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+API_KEY = os.environ.get('API_KEY', None)
+
 @app.route("/")
 def chat():
-    return {"status": "working"}
+    return {"status": "working", "API_KEY": API_KEY}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
