8907f3855741b8a71f5da3b255c33a8573eb3244
Andrew Graves <agraves@codecademy.com>
2025-01-03 12:39:54 -0500

    basic WS route setup

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from "express";
 import { themes } from "./themes";
+import expressWs from "express-ws";
+import { WebSocket } from "ws";
+import cors from "cors";
 
-const app = express();
+const expWs = expressWs(express());
+const app = expWs.app;
+
 const port = process.env.PORT || 9999;
+
+app.use(cors({ origin: "http://localhost" }));
 app.use("/static", express.static("public"));
 
 app.get("/status", (_: Request, res: Response) => {
@@ -11,6 +18,13 @@ app.get("/status", (_: Request, res: Response) => {
 
 app.get("/themes", (_: Request, res: Response) => {
   res.json(themes);
+});
+
+app.ws("/story", (ws: WebSocket, _: Request) => {
+  ws.on("message", (msg: string) => {
+    console.log("received: %s", msg);
+    ws.send('{"text":"Hello, world!"}');
+  });
 });
 
 app.listen(port, () => {
